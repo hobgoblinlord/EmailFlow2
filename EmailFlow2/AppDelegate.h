d7e2dfc09b91ddb34aa677c9bc623fb586739fb5
hobgoblinlord <nproulx@incubate.co>
2014-01-19 02:10:37 -0500

    Initial Commit

---
+++ EmailFlow2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  EmailFlow2
+//
+//  Created by Noel Proulx on 1/19/14.
+//  Copyright (c) 2014 Noel Proulx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EmailFlow2Tests/EmailFlow2Tests.m
@@ -0,0 +1,34 @@
+//
+//  EmailFlow2Tests.m
+//  EmailFlow2Tests
+//
+//  Created by Noel Proulx on 1/19/14.
+//  Copyright (c) 2014 Noel Proulx. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface EmailFlow2Tests : XCTestCase
+
+@end
+
+@implementation EmailFlow2Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
